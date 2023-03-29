37111864caed5315e2b4fb4a4e83c41549b18682
Walfred-MA <90290821+Walfred-MA@users.noreply.github.com>
2023-03-29 16:30:04 -0700

    Update PriorData.cpp

--- PriorData.cpp
+++ PriorData.cpp
@@ -231,7 +231,7 @@ void PriorData::LoadAlleles(PriorChunk &Chunk)
 void PriorData::LoadNorm(PriorChunk &Chunk)
 {
     string StrLine;
-    StrLine.resize(curr_genenum *curr_genenum );
+    StrLine.resize(Chunk.genenum *Chunk.genenum);
     
     if (!file.nextLine_norm(StrLine))
     {
