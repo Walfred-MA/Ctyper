4f3f74eba1003fb88d1bb76ccecc434df5995e39
Walfred-MA <90290821+Walfred-MA@users.noreply.github.com>
2023-03-29 18:56:23 -0700

    Update Processor.hpp

--- Processor.hpp
+++ Processor.hpp
@@ -320,7 +320,7 @@ void Processor<ksize>::Run()
 	cout<<"reading all kmer targets"<<endl;
  
     Counter.LoadRegion(regions);
-    totalkmers = Counter.read_target(matrixfile.c_str(), 2 * priordata_manager.totalkmers);
+    totalkmers = Counter.read_target(matrixfile.c_str(), 3 * priordata_manager.totalkmers);
     
     
 	cout<<"finishing reading targets, start genotyping"<<endl;
