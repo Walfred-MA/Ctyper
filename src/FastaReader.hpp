b18e9e58a4552c25b5f14a301ffcceb0ed4e2ce6
Walfred-MA <90290821+Walfred-MA@users.noreply.github.com>
2024-11-02 04:05:41 -0700

    Update FastaReader.hpp

--- src/FastaReader.hpp
+++ src/FastaReader.hpp
@@ -1,9 +1,10 @@
 //
-//  Fasta.hpp
-//  CTyper
+//  Created by Walfred (Wangfei) MA at the University of Southern California,
+//  Mark Chaisson Lab on 2/13/23.
 //
-//  Created by Wangfei MA on 2/13/23.
-//
+//  Licensed under the MIT License. 
+//  If you use this code, please cite our work.
+//   
 
 #ifndef Fasta_hpp
 #define Fasta_hpp
